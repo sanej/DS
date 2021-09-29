ebcb7a3246ec3be348031a789d32a56874539e9a
Sanej <sanejbandgar@gmail.com>
2021-09-28 17:44:07 -0700

    Graph Changes
    added new changes

--- 01-PY/FileRead.py
+++ 01-PY/FileRead.py
@@ -1,10 +1,17 @@
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'Scores.txt')
+
 highestscore = 0
 
-result_f = open("/Users/SanejTej/Documents/06-GitRepos/SanejGit/DataSciencePlayGround/01-PY/Scores.txt")
+#result_f = open("/Users/SanejTej/Documents/06-GitRepos/SanejGit/DataSciencePlayGround/01-PY/Scores.txt")
+result_f = open(my_file)
 
 for line in result_f:
-    if line > highestscore:
-        highestscore = line
+    line = line.split(" ")
+    score = float(line[1])
+    if score > highestscore:
+        highestscore = score
 result_f.close()
 print("The higest score was:")
 print(highestscore)
